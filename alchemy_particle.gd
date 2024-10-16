cd9c42753dd087a50d776d0ee13449d25246772b
secretmeister2 <26khoffman@isstavanger.no>
2024-10-16 21:55:00 +0200

    rapier added

---
+++ alchemy_particle.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+@export var viscosity =1
+@export var density = 1
+@export var effects = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- mixer.gd
+++ mixer.gd
@@ -1,5 +1,4 @@
 extends Node2D
-var animated = false
 var timer = 0
 func _process(delta: float) -> void:
 	pass
